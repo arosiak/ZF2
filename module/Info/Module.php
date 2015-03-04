2819e2081f90d3984f8b423976e62e0889bd2df0
Artur <a1627@wp.pl>
2015-03-04 21:45:44 +0100

    update

--- module/Info/Module.php
+++ module/Info/Module.php
@@ -19,6 +19,7 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        // Komentarz
     }
 
     public function getConfig()
